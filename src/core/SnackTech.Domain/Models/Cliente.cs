4624aedbbe32ca7166a703cca3e25500970b4fec
Rafael <rafael.dgs.1993@gmail.com>
2024-07-13 17:23:47 -0300

    adicionando dominio de pedido e pedido item. Cobertura de testes em 100% do Dominio

--- src/core/SnackTech.Domain/Models/Cliente.cs
+++ src/core/SnackTech.Domain/Models/Cliente.cs
@@ -4,19 +4,16 @@ namespace SnackTech.Domain.Models
 {
     public class Cliente
     {
-        public Guid Id {get; private set;}        
+        public Pessoa Pessoa {get; private set;}  
         public string Email {get; private set;}
-        public string Nome {get; private set;}
         public string CPF {get; private set;}
 
         public Cliente(Guid id, string nome, string email, string cpf){
-            CustomGuards.AgainstStringNullOrWhiteSpace(nome, nameof(nome));
+            Pessoa = new Pessoa(id,nome);
+            
             EmailGuard.AgainstInvalidEmail(email, nameof(email));
             CpfGuard.AgainstInvalidCpf(cpf, nameof(cpf));
-
-            Id = id;
             Email = email;
-            Nome = nome;
             CPF = cpf;
         }
 
---
+++ src/tests/SnackTech.Domain.Tests/ModelsTests/PedidoItemTests.cs
@@ -0,0 +1,94 @@
+using SnackTech.Domain.Enums;
+using SnackTech.Domain.Models;
+
+namespace SnackTech.Domain.Tests.ModelsTests
+{
+    public class PedidoItemTests
+    {
+        [Fact]
+        public void CreatePedidoItemWithObservation()
+        {
+            var produto = CriarProduto(CategoriaProduto.Bebida, "Refrigerante", 5);
+            var pedidoItem = CriarPedidoItem(produto,1,"observação");
+            Assert.Equal("observação",pedidoItem.Observacao);
+        }
+
+        [Fact]
+        public void CreatePedidoItemWithNullObservation(){
+            var produto = CriarProduto(CategoriaProduto.Bebida, "Refrigerante", 5);
+            var pedidoItem = CriarPedidoItem(produto,1,null);
+            Assert.Equal(string.Empty,pedidoItem.Observacao);
+        }
+
+        [Fact]
+        public void AtualizarDadosItemWithNullProduct(){
+            try{
+                var produto = CriarProduto(CategoriaProduto.Bebida, "Refrigerante", 5);
+                var pedidoItem = CriarPedidoItem(produto,1,"observação");
+
+                pedidoItem.AtualizarDadosItem(null,1,"sabor morango");
+
+                Assert.Fail("Permitiu atualizar dados do pedido item com produto nulo");
+            }
+            catch(ArgumentException ex){
+                Assert.Equal("produto não pode ser nulo. (Parameter 'produto')",ex.Message);
+            }
+            catch(Exception ex){
+                Assert.Fail($"Lançou uma exception inesperada. {ex.Message}");
+            }
+        }
+
+        [Fact]
+        public void AtualizarDadosItemWithQuantityLessThanOne(){
+            try{
+                var produto = CriarProduto(CategoriaProduto.Bebida, "Refrigerante", 5);
+                var pedidoItem = CriarPedidoItem(produto,1,"observação");
+
+                var novoProduto = CriarProduto(CategoriaProduto.Sobremesa,"Sorvete",6);
+                pedidoItem.AtualizarDadosItem(novoProduto,0,"sabor morango");
+
+                Assert.Fail("Permitiu atualizar dados do pedido item com quantidade menor que 1");
+            }
+            catch(ArgumentException ex){
+                Assert.Equal("quantidade precisa ser maior do que zero. (Parameter 'quantidade')",ex.Message);
+            }
+            catch(Exception ex){
+                Assert.Fail($"Lançou uma exception inesperada. {ex.Message}");
+            }
+        }
+
+        [Fact]
+        public void AtualizarDadosItemWithSuccess(){
+            try{
+                var produto = CriarProduto(CategoriaProduto.Bebida, "Refrigerante", 5);
+                var pedidoItem = CriarPedidoItem(produto,1,"observação");
+
+                var novoProduto = CriarProduto(CategoriaProduto.Sobremesa,"Sorvete",6);
+                pedidoItem.AtualizarDadosItem(novoProduto,1,"sabor morango");
+
+                Assert.NotNull(pedidoItem.Produto);
+                Assert.Equal(1,pedidoItem.Quantidade);
+                Assert.Equal(1,pedidoItem.Sequencial);
+                Assert.Equal("sabor morango",pedidoItem.Observacao);
+
+                var pedidoItemProduto = pedidoItem.Produto;
+                Assert.NotNull(pedidoItemProduto);
+                Assert.Equal("Sorvete",pedidoItemProduto.Nome);
+                Assert.Equal(CategoriaProduto.Sobremesa,pedidoItemProduto.Categoria);
+                Assert.Equal(6,pedidoItemProduto.Valor);
+            }
+            catch(Exception ex){
+                Assert.Fail($"Lançou uma exception inesperada. {ex.Message}");
+            }
+        }
+
+        private PedidoItem CriarPedidoItem(Produto produto, int quantidade, string observacao)
+            => new PedidoItem(1,produto,quantidade,observacao);
+
+        private Produto CriarProduto(CategoriaProduto categoria,string nome, decimal valor)
+            => new Produto(categoria,nome,nome,valor);
+
+        private Cliente CriarCliente()
+            => new Cliente("Nome Cliente","email@gmail.com","89934782014");
+    }
+}
