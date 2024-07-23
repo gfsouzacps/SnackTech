22d9db00c6021f5e7757974fa3bc09ed26c52c56
Dayvid Ribeiro Correia <dayvid@sorteonline.com.br>
2024-07-22 21:11:12 -0300

    aplicando migration

--- src/driving/SnackTech.API/Program.cs
+++ src/driving/SnackTech.API/Program.cs
@@ -1,4 +1,5 @@
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.EntityFrameworkCore;
+using SnackTech.Adapter.DataBase.Context;
 using SnackTech.API.Configuration.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,7 +13,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<RepositoryDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+await using var dbContext = scope.ServiceProvider.GetRequiredService<RepositoryDbContext>();
+await dbContext.Database.MigrateAsync();
 
 app.UseCustomHealthChecks();
 app.UseSwagger();
