5441d8a30f02f5e6c9bfc55a1fa890ed04cb30ba
Adriano Costa <adriano.dmcosta@gmail.com>
2024-07-25 21:04:29 -0300

    Adicionar testes para PedidoService.AtualizarPedido

--- src/tests/SnackTech.Application.Tests/UseCasesTests/PedidoServiceTests.cs
+++ src/tests/SnackTech.Application.Tests/UseCasesTests/PedidoServiceTests.cs
@@ -1,8 +1,6 @@
 using Microsoft.Extensions.Logging;
 using Moq;
 using SnackTech.Application.DTOs.Pedido;
-using SnackTech.Application.DTOs.Produto;
-using SnackTech.Application.Interfaces;
 using SnackTech.Application.UseCases;
 using SnackTech.Domain.Contracts;
 using SnackTech.Domain.Enums;
@@ -259,10 +257,10 @@ namespace SnackTech.Application.Tests.UseCasesTests
             var identificacaoString = identificacao.ToString();
             var cliente = new Cliente(Guid.NewGuid(), "Nome completo", "email@gmail.com", "582.202.320-72");
 
-            Produto produto = new Produto(CategoriaProduto.Lanche,"descricao","Produto",20);
+            Produto produto = new Produto(CategoriaProduto.Lanche, "descricao", "Produto", 20);
             var pedidoItem = new PedidoItem(1, produto, 2, "");
             var pedido = new Pedido(identificacao, DateTime.Now, StatusPedido.Iniciado, cliente, new List<PedidoItem>() { pedidoItem });
-            
+
             pedidoRepository.Setup(p => p.PesquisarPorId(It.IsAny<Guid>()))
                             .ReturnsAsync(pedido);
 
@@ -299,7 +297,7 @@ namespace SnackTech.Application.Tests.UseCasesTests
         public async Task FinalizarPedidoParaPagamentoSemItensNotFounded()
         {
             var identificacao = Guid.NewGuid();
-            
+
             pedidoRepository.Setup(p => p.PesquisarPorId(It.IsAny<Guid>()))
                             .ReturnsAsync(default(Pedido));
 
@@ -426,6 +424,82 @@ namespace SnackTech.Application.Tests.UseCasesTests
 
             Assert.True(resultado.IsSuccess());
             Assert.Null(resultado.Exception);
+        }
+        [Fact]
+        public async Task AtualizarPedidoRemovendoItensExistentesWithSuccess()
+        {
+            var identificacao = Guid.NewGuid();
+            var identificacaoString = identificacao.ToString();
+            var cliente = new Cliente(Guid.NewGuid(), "Nome completo", "email@gmail.com", "582.202.320-72");
+            var produto = new Produto(CategoriaProduto.Lanche, "descricao", "Produto", 20);
+            var produto2 = new Produto(CategoriaProduto.Bebida, "descricao", "Produto2", 10);
+            produtoRepository.Setup(p => p.PesquisarPorId(produto.Id))
+                            .ReturnsAsync(produto);
+            produtoRepository.Setup(p => p.PesquisarPorId(produto2.Id))
+                            .ReturnsAsync(produto2);
+
+            var pedidoItem1 = new PedidoItem(1, produto, 2, "remover");
+            var pedidoItem2 = new PedidoItem(2, produto2, 1, "manter");
+            var pedido = new Pedido(identificacao, DateTime.Now, StatusPedido.AguardandoPagamento, cliente, new List<PedidoItem>() { pedidoItem1, pedidoItem2 });
+            pedidoRepository.Setup(p => p.PesquisarPorId(It.IsAny<Guid>()))
+                            .ReturnsAsync(pedido);
+            pedidoRepository.Setup(p => p.AtualizarPedido(It.IsAny<Pedido>()))
+                            .Callback<Pedido>((pedidoAtualizado) =>
+                            {
+                                Assert.Equal(1, pedidoAtualizado.Itens.Count);
+                                Assert.Equal(pedidoItem2.Sequencial, pedidoAtualizado.Itens.First().Sequencial);
+                                Assert.Equal(pedidoItem2.Observacao, pedidoAtualizado.Itens.First().Observacao);
+                            })
+                            .Returns(Task.CompletedTask);
+
+            var pedidoItemAtualizado = new AtualizacaoPedidoItem
+            {
+                Sequencial = pedidoItem2.Sequencial,
+                IdentificacaoProduto = pedidoItem2.Produto.Id.ToString(),
+                Quantidade = pedidoItem2.Quantidade,
+                Observacao = pedidoItem2.Observacao,
+            };
+
+            var AtualizacaoPedido = new AtualizacaoPedido
+            {
+                Identificacao = identificacaoString,
+                PedidoItens = new List<AtualizacaoPedidoItem> { pedidoItemAtualizado }
+            };
+
+            var resultado = await pedidoService.AtualizarPedido(AtualizacaoPedido);
+
+            Assert.True(resultado.IsSuccess());
+            Assert.Null(resultado.Exception);
+        }
+
+        [Fact]
+        public async Task AtualizarPedidoWithInvalidGuid()
+        {
+            var AtualizacaoPedido = new AtualizacaoPedido
+            {
+                Identificacao = "aaaaaaaaaaaaaaa",
+                PedidoItens = new List<AtualizacaoPedidoItem>()
+            };
+
+            var resultado = await pedidoService.AtualizarPedido(AtualizacaoPedido);
+
+            Assert.False(resultado.IsSuccess());
+            Assert.Null(resultado.Exception);
+            Assert.Contains($"Identificacao não é um Guid válido", resultado.Message);
+        }
+
+        [Fact]
+        public async Task AtualizarPedidoException()
+        {
+            pedidoRepository.Setup(p => p.PesquisarPorId(It.IsAny<Guid>()))
+                            .ThrowsAsync(new Exception("Erro inesperado"));
+            var identificacao = Guid.NewGuid().ToString();
+
+            var resultado = await pedidoService.FinalizarPedidoParaPagamento(identificacao);
+
+            Assert.False(resultado.IsSuccess());
+            Assert.NotNull(resultado.Exception);
+            Assert.Contains($"Erro inesperado", resultado.Message);
         }
     }
 }
