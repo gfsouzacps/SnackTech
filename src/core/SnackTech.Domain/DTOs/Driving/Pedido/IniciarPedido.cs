a7593dae1e2f7d4708965c3916a569c424bae742
Rafael <rafael.dgs.1993@gmail.com>
2024-08-11 19:33:50 -0300

    padronizando verbos, rotas e parametros

---
+++ src/core/SnackTech.Domain/DTOs/Driving/Pedido/IniciarPedido.cs
@@ -0,0 +1,8 @@
+
+namespace SnackTech.Domain.DTOs.Driving.Pedido
+{
+    public class IniciarPedido
+    {
+         public string Cpf {get; set;} = string.Empty;        
+    }
+}
--- src/driving/SnackTech.API/Controllers/PedidosController.cs
+++ src/driving/SnackTech.API/Controllers/PedidosController.cs
@@ -18,30 +18,30 @@ namespace SnackTech.API.Controllers
         [ProducesResponseType<Guid>(StatusCodes.Status200OK)]
         [ProducesResponseType<ErrorResponse>(StatusCodes.Status400BadRequest)]
         [ProducesResponseType<ErrorResponse>(StatusCodes.Status500InternalServerError)]
-        [Route("iniciar")]
+        [Route("")]
         [SwaggerOperation(Summary = "Inicia um novo pedido a partir do CPF de um cliente previamente cadastrado no banco. Informe null para iniciar um novo pedido do cliente padrão")]
         [SwaggerResponse(StatusCodes.Status200OK, "Retorna o identificador do novo pedido", typeof(Guid))]
-        public async Task<IActionResult> IniciarPedido([FromBody] string cpfCliente)
-            => await CommonExecution("Pedidos.IniciarPedido", pedidoService.IniciarPedido(cpfCliente));
+        public async Task<IActionResult> IniciarPedido([FromBody] IniciarPedido pedidoIniciado)
+            => await CommonExecution("Pedidos.IniciarPedido", pedidoService.IniciarPedido(pedidoIniciado.Cpf));
 
-        [HttpPost]
+        [HttpPut]
         [Consumes(MediaTypeNames.Application.Json)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType<ErrorResponse>(StatusCodes.Status400BadRequest)]
         [ProducesResponseType<ErrorResponse>(StatusCodes.Status500InternalServerError)]
-        [Route("atualizar")]
+        [Route("")]
         [SwaggerOperation(Summary = "Atualiza o pedido (itens anexos) que ainda não esteja no status aguardando pagamento")]
         public async Task<IActionResult> AtualizarPedido([FromBody] AtualizacaoPedido atualizacaoPedido)
             => await CommonExecution("Pedidos.AtualizarPedido", pedidoService.AtualizarPedido(atualizacaoPedido));
 
-        [HttpPost]
+        [HttpPatch]
         [Consumes(MediaTypeNames.Application.Json)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType<ErrorResponse>(StatusCodes.Status400BadRequest)]
         [ProducesResponseType<ErrorResponse>(StatusCodes.Status500InternalServerError)]
-        [Route("finalizar-para-pagamento")]
+        [Route("finalizar-para-pagamento/{identificacao:guid}")]
         [SwaggerOperation(Summary = "Finaliza o pedido com o identificador informado e o coloca na situação de aguardando pagamento")]
-        public async Task<IActionResult> FinalizarPedidoParaPagamento([FromBody] string identificacao)
+        public async Task<IActionResult> FinalizarPedidoParaPagamento([FromRoute] string identificacao)
             => await CommonExecution("Pedidos.FinalizarPedidoParaPagamento", pedidoService.FinalizarPedidoParaPagamento(identificacao));
 
         [HttpGet]
@@ -66,9 +66,9 @@ namespace SnackTech.API.Controllers
         [ProducesResponseType<RetornoPedido>(StatusCodes.Status200OK)]
         [ProducesResponseType<ErrorResponse>(StatusCodes.Status400BadRequest)]
         [ProducesResponseType<ErrorResponse>(StatusCodes.Status500InternalServerError)]
-        [Route("ultimo-pedido-cliente")]
+        [Route("ultimo-pedido-cliente/{cpfCliente}")]
         [SwaggerOperation(Summary = "Retorna o pedido mais recente do cliente com CPF informado. Não é permitida a consulta do último pedido do cliente padrão")]
-        public async Task<IActionResult> BuscarUltimoPedidoCliente([FromQuery] string cpfCliente)
+        public async Task<IActionResult> BuscarUltimoPedidoCliente([FromRoute] string cpfCliente)
             => await CommonExecution("Pedidos.ListarPedidosParaPagamento", pedidoService.BuscarUltimoPedidoCliente(cpfCliente));
     }
 }
