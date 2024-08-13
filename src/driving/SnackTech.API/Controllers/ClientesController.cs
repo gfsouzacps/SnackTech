3764a55acc77ffef69534a8b8857172c46b57bfc
gfsouzacps <gui240799@outlook.com>
2024-08-13 19:52:25 -0300

    Ajuste na documentação dos endpoints

--- src/driving/SnackTech.API/Controllers/ClientesController.cs
+++ src/driving/SnackTech.API/Controllers/ClientesController.cs
@@ -13,6 +13,14 @@ namespace SnackTech.API.Controllers
     {
         private readonly IClienteService clienteService = clienteService;
 
+        /// <summary>
+        /// Cria um novo cliente.
+        /// </summary>
+        /// <remarks>
+        /// Cria um nova cliente no sistema.
+        /// </remarks>
+        /// <param name="cadastroCliente">Os dados do cliente a ser criado.</param>
+        /// <returns>Um <see cref="IActionResult"/> representando o resultado da operação.</returns>
         [HttpPost]
         [Consumes(MediaTypeNames.Application.Json)]
         [ProducesResponseType<RetornoCliente>(StatusCodes.Status200OK)]
@@ -20,8 +28,16 @@ namespace SnackTech.API.Controllers
         [ProducesResponseType<ErrorResponse>(StatusCodes.Status500InternalServerError)]
         [SwaggerOperation(Summary = "Cadastra um novo cliente no sistema")]
         public async Task<IActionResult> Post([FromBody] CadastroCliente cadastroCliente)
-            => await CommonExecution("Clientes.Post",clienteService.Cadastrar(cadastroCliente));
+            => await CommonExecution("Clientes.Post", clienteService.Cadastrar(cadastroCliente));
 
+        /// <summary>
+        /// Retorna o cliente com o CPF informado.
+        /// </summary>
+        /// <remarks>
+        /// Retorna as informações de um cliente baseado no CPF fornecido.
+        /// </remarks>
+        /// <param name="cpf">O CPF do cliente a ser pesquisado.</param>
+        /// <returns>Um <see cref="IActionResult"/> contendo o cliente encontrado ou um erro correspondente.</returns>
         [HttpGet]
         [Route("{cpf}")]
         [ProducesResponseType<RetornoCliente>(StatusCodes.Status200OK)]
@@ -29,8 +45,15 @@ namespace SnackTech.API.Controllers
         [ProducesResponseType<ErrorResponse>(StatusCodes.Status500InternalServerError)]
         [SwaggerOperation(Summary = "Retorna o cliente com o CPF informado")]
         public async Task<IActionResult> GetByCpf([FromRoute] string cpf)
-            => await CommonExecution("Clientes.GetByCpf",clienteService.IdentificarPorCpf(cpf));
+            => await CommonExecution("Clientes.GetByCpf", clienteService.IdentificarPorCpf(cpf));
 
+        /// <summary>
+        /// Retorna o cliente padrao.
+        /// </summary>
+        /// <remarks>
+        /// Retorna as informações do cliente padr?o do sistema.
+        /// </remarks>
+        /// <returns>Um <see cref="IActionResult"/> contendo o cliente encontrado ou um erro correspondente.</returns>
         [HttpGet]
         [Route("cliente-padrao")]
         [ProducesResponseType<RetornoCliente>(StatusCodes.Status200OK)]
@@ -38,6 +61,6 @@ namespace SnackTech.API.Controllers
         [ProducesResponseType<ErrorResponse>(StatusCodes.Status500InternalServerError)]
         [SwaggerOperation(Summary = "Retorna o cliente padrao")]
         public async Task<IActionResult> GetDefaultClient()
-            => await CommonExecution("Clientes.GetDefaultClient",clienteService.SelecionarClientePadrao());
+            => await CommonExecution("Clientes.GetDefaultClient", clienteService.SelecionarClientePadrao());
     }
 }
