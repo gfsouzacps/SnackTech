a15c608b5582640c424b5809b10c6dafe3dd8bf7
Adriano Costa <adriano.dmcosta@gmail.com>
2024-09-29 12:17:31 -0300

    Retirar campo não usado do DTO

--- src/common/SnackTech.Common/Dto/Api/PedidoItemAtualizacaoDto.cs
+++ src/common/SnackTech.Common/Dto/Api/PedidoItemAtualizacaoDto.cs
@@ -2,10 +2,9 @@ namespace SnackTech.Common.Dto.Api;
 
 public class PedidoItemAtualizacaoDto
 {
-    //Informar o Id caso seja uma atualização do item ou null para item novo no pedido
+    //Informar o Id caso seja uma atualizaï¿½ï¿½o do item ou null para item novo no pedido
     public string? Id { get; set; }
     public int Quantidade { get; set; }
-    public decimal Valor { get; set; }
     public string Observacao { get; set; }
     public string ProdutoId { get; set; }
 }
